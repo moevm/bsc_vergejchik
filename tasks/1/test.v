34155be77a0bf87d10b2ae37d92b78188cbb919b
gglebaty <gleb.verg@mail.ru>
2020-03-11 23:31:24 +0300

    Assertion

--- tasks/1/test.v
+++ tasks/1/test.v
@@ -6,42 +6,57 @@ module and_test;
 	localparam period = 10;
 
 	AND and_inst(a, b, c, sum);
-
+	
+	`define assert(signal, expected) \
+        if (signal !== expected) begin \
+            $display("ASSERTION FAILED"); \
+            $finish; \
+        end
 
 	initial
+
 		begin
 			a = 0;
 			b = 0;
 			c = 0;
 			#period;
+			`assert(sum, 0)
 	
 			c = 1;
 			#period;
+			`assert(sum, 0)
 			
 			b = 1;
 			c = 0;
 			#period;
+			`assert(sum, 0)
 
 			c = 1;
 			#period;
+			`assert(sum, 0)
 
 			a = 1;
 			b = 0;
 			c = 0;
 			#period;
+			`assert(sum, 0)
 			
 			c = 1;
 			#period;
+			`assert(sum, 0)
 			
 			b = 1;
 			c = 0;
 			#period;
+			`assert(sum, 0)
 			
 			c = 1;
 			#period;
+			`assert(sum, 1)
 			
 		end
-
+		
 	initial
 		$monitor(a,, b,, c,, sum);
+
 endmodule
