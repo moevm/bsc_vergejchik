42cc19525ed724809e1a937e1f607442a24c3670
gglebaty <gleb.verg@mail.ru>
2020-05-31 17:47:51 +0300

    Xqueue

---
+++ xqueue/xqueue/submission_queue/migrations/0002_stop_updating_arrival_time.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from __future__ import absolute_import
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    replaces = [('queue', '0002_stop_updating_arrival_time')]
+
+    dependencies = [
+        ('submission_queue', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='submission',
+            name='arrival_time',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+    ]
---
+++ xqueue/xqueue/xqueue/urls.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^xqueue/', include('submission_queue.urls')),
+]
